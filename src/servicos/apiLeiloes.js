2a75d1913e14bc8c5aab9d9ef0b1e74ad5ed603f
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-15 21:59:34 -0300

    change ip

--- src/servicos/apiLeiloes.js
+++ src/servicos/apiLeiloes.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // create axios
 const instance = axios.create({
-  baseURL: 'http://192.168.1.3:3000/',
+  baseURL: 'http://192.168.100.65:3000/',
   timeout: 1000,
 });
 
