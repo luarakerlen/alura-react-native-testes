8c9a40613215f868a6a56cdff213f877263fd032
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-16 17:28:42 -0300

    chore: add clear on mock

--- __tests__/repositorio/leilao.test.js
+++ __tests__/repositorio/leilao.test.js
@@ -35,12 +35,17 @@ const mockRequisicaoErro = () => {
 };
 
 describe('repositorio/leilao', () => {
+	beforeEach(() => {
+		apiLeiloes.get.mockClear();
+	});
 	describe('obtemLeiloes', () => {
 		it('deve retornar uma lista de leilões', async () => {
 			apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes));
 
 			const leiloes = await obtemLeiloes();
 			expect(leiloes).toEqual(mockLeiloes);
+			expect(apiLeiloes.get).toHaveBeenCalledWith('/leiloes');
+			expect(apiLeiloes.get).toHaveBeenCalledTimes(1);
 		});
 
 		it('deve retornar uma lista vazia quando a requisição falhar', async () => {
@@ -48,6 +53,8 @@ describe('repositorio/leilao', () => {
 
 			const leiloes = await obtemLeiloes();
 			expect(leiloes).toEqual([]);
+			expect(apiLeiloes.get).toHaveBeenCalledWith('/leiloes');
+			expect(apiLeiloes.get).toHaveBeenCalledTimes(1);
 		});
 	});
 });
